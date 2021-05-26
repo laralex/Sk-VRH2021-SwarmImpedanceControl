8350a0ce1ca5e6f9101beed2fc671746d47f00ad
Alex Larionov <alxs.larionov@gmail.com>
2021-05-26 20:59:09 +0300

    Add impedance control, with extra feature to rotate the swarm

---
+++ Assets/Scripts/ImpedanceController.cs
@@ -0,0 +1,140 @@
+﻿// using MathNet.Numerics.LinearAlgebra;
+// using MathNet.Numerics.LinearAlgebra.Single;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Collections;
+using UnityEngine;
+
+public class ImpedanceController : MonoBehaviour
+{
+    public bool printLogs = false;
+    public Transform obstacle = null;
+    public float kGain = 1.0f;
+    public float mass = 1.0f;
+    public float damping = 2.0f;
+    public float stiffness = 1.0f;
+    public Vector3 correctionLimit = new Vector3(1000f, 1000f, 1000f);
+    private Rigidbody rootBody;
+    private float runTime = 20.0f;
+    private float a = 0.0f;
+    private float b = 0.0f;
+    private float c = 0.0f;
+
+    // private Matrix<float> PoseImp = null;
+    // private Matrix<float> PoseError = null;
+    
+    private Vector3 ForceExt;
+    // private Matrix<float> AN = null;
+    // private Matrix<float> BN = null;
+    // private Matrix<float> AD = null;
+    // private Matrix<float> BD = null;
+    private float A11, A12, A21, A22;
+    private float B1, B2;
+    private Vector3 positionCorrection = Vector3.zero;
+    private Vector3 velocityCorrection = Vector3.zero;
+    private Vector3 prevPosition, prevVelocity;
+    private Vector3 obstaclePrevPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rootBody = GetComponent<Rigidbody>();
+        if (this.rootBody == null) {
+            this.rootBody = GetComponentInParent<Rigidbody>();
+        }
+        
+        this.prevPosition = rootBody.transform.localPosition;
+        this.prevVelocity = rootBody.velocity;
+        this.obstaclePrevPosition = transform.InverseTransformPoint(obstacle.position);
+    }
+
+    public (Vector3, Vector3) GetCorrection() {
+        var deltaPosition = rootBody.transform.localPosition - this.prevPosition;
+        var deltaVelocity = Vector3.zero; // rootBody.velocity - this.prevVelocity;
+        var newObstaclePos = transform.InverseTransformPoint(obstacle.position);
+        var obstacleV = (newObstaclePos - this.obstaclePrevPosition)/Time.deltaTime;
+        this.obstaclePrevPosition = newObstaclePos;
+        this.prevPosition = rootBody.transform.localPosition;
+        this.prevVelocity = rootBody.velocity;
+        return GetCorrection(deltaPosition, deltaVelocity, obstacleV);
+    }
+    public (Vector3, Vector3) GetCorrection(Vector3 deltaPosition, Vector3 deltaTargetV, Vector3 obstacleV)
+    {
+        InitState();
+        ForceEvaluation(obstacleV);
+        // AD = DenseMatrix.Create(2, 2, 0.0f);
+        // BD = DenseMatrix.Create(2, 1, 0.0f);
+        float D = Mathf.Pow(-a, 2) + 4 * b; // - 4*b
+        if (D >= 0)
+        {
+            float lambda = (a + Mathf.Sqrt(D)) / 2;
+
+            // AD[0, 0] = 1 - lambda * runTime;
+            // AD[0, 1] = runTime;
+            // AD[1, 0] = -b * runTime;
+            // AD[1, 1] = 1 - lambda * runTime - a * runTime;
+            // AD = expLambdaT * AD;
+            // BD[0, 0] = expLambdaT * (1 - lambda * runTime) - 1;
+            // BD[1, 0] = -expLambdaT * b * runTime;
+            // BD = -c / b * BD;
+            float expLambdaT = Mathf.Exp(lambda * runTime);
+            A12 = expLambdaT * (runTime);
+            A11 = expLambdaT * (1 - lambda * runTime);
+            A21 = expLambdaT * (-b * runTime);
+            A22 = expLambdaT * (1 - lambda * runTime - a * runTime);
+            B1 = -c / b * (expLambdaT * (1 - lambda * runTime) - 1f);
+            B2 = -c / b * (-expLambdaT * b * runTime);
+            //PoseImp = AD * PoseError + BD * ForceExt;
+            // Matrix<float> AxisErrorX = DenseMatrix.Create(2, 1, 0.0f);
+            // Matrix<float> AxisErrorY = DenseMatrix.Create(2, 1, 0.0f);
+            // Matrix<float> AxisErrorZ = DenseMatrix.Create(2, 1, 0.0f);
+
+            float posCorrectionX = A11 * deltaPosition.x + A12 * deltaTargetV.x + B1 * ForceExt.x;
+            float velCorrectionX = A21 * deltaPosition.x + A22 * deltaTargetV.x + B2 * ForceExt.x;
+            // AxisErrorX[0, 0] = deltaPosition.x;
+            // AxisErrorX[1, 0] = deltaTargetV.x;
+            // AxisErrorX = AD * AxisErrorX + BD * ForceExt.x;
+
+            float posCorrectionY = A11 * deltaPosition.y + A12 * deltaTargetV.y + B1 * ForceExt.y;
+            float velCorrectionY = A21 * deltaPosition.y + A22 * deltaTargetV.y + B2 * ForceExt.y;
+            // AxisErrorY[0, 0] = deltaPosition.y;
+            // AxisErrorY[1, 0] = deltaTargetV.y;
+            // AxisErrorY = AD * AxisErrorY + BD * ForceExt.y;
+
+            float posCorrectionZ = A11 * deltaPosition.z + A12 * deltaTargetV.z + B1 * ForceExt.z;
+            float velCorrectionZ = A21 * deltaPosition.z + A22 * deltaTargetV.z + B2 * ForceExt.z;
+            // AxisErrorZ[0, 0] = deltaPosition.z;
+            // AxisErrorZ[1, 0] = deltaTargetV.z;
+            // AxisErrorZ = AD * AxisErrorZ + BD * ForceExt.z;
+
+            if (printLogs) {
+                // print($"{AD[0,0]} {AD[0,1]} {AD[1,0]} {AD[1,1]}");
+                // print($"{AxisErrorX[0,0]}, {AxisErrorY[0,0]}, {AxisErrorZ[0,0]}");
+            }
+            positionCorrection = new Vector3(
+                Mathf.Clamp(posCorrectionX, -correctionLimit.x, correctionLimit.x),
+                Mathf.Clamp(posCorrectionY, -correctionLimit.y, correctionLimit.y),
+                Mathf.Clamp(posCorrectionZ, -correctionLimit.z, correctionLimit.z));
+            velocityCorrection = new Vector3(velCorrectionX, velCorrectionY, velCorrectionZ);
+
+            return (positionCorrection, velocityCorrection);
+        }
+        Debug.Log("Problems");
+        return (new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f));
+    }
+
+    void InitState()
+    {
+        a = -damping / mass;
+        b = -stiffness / mass;
+        c = 1 / mass;
+        runTime = Time.deltaTime;
+    }
+
+    void ForceEvaluation(Vector3 obsV)
+    {
+        ForceExt = obsV * kGain;
+    }
+
+}
---
+++ Assets/Scripts/SwarmController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Mass {
+        get => _mass;
+        set {
+            _mass = value;
+            UpdateLinksParameter(link => link.mass = _mass);
+        }
+    }
+    public float Damping {
+        get => _damping;
+        set {
+            _damping = value;
+            UpdateLinksParameter(link => link.damping = _damping);
+        }
+    }
+    public float Stiffness {
+        get => _stiffness;
+        set {
+            _stiffness = value;
+            UpdateLinksParameter(link => link.stiffness = _stiffness);
+        }
+    }
+    public float Gain {
+        get => _gain;
+        set {
+            _gain = value;
+            UpdateLinksParameter(link => link.kGain = _gain);
+        }
+    }
+
+    public Vector3 defaultCorrectionLimit;
+    public Transform nearDestination; // some target to follow (but not too far from centroid)
+    public GameObject[] drones = new GameObject[4];
+    [SerializeField]
+    public Vector2[] desiredDistances = new Vector2[4];
+    public Transform SwarmCentroid;
+    private List<ImpedanceController[]> links =  new List<ImpedanceController[]>();
+    [SerializeField]
+    private float _mass = 1f, _damping = 2f, _stiffness = 1f, _gain = 1f;
+    void Start()
+    {
+        // head
+        links.Add(new ImpedanceController[] {
+            SetupLink(drones[0].AddComponent<ImpedanceController>(), nearDestination.transform, logs: true)});
+        // right
+        links.Add(new ImpedanceController[] {
+            SetupLink(drones[1].AddComponent<ImpedanceController>(), drones[0].transform)});
+        // left
+        links.Add(new ImpedanceController[] {
+            SetupLink(drones[2].AddComponent<ImpedanceController>(), drones[0].transform),
+            SetupLink(drones[2].AddComponent<ImpedanceController>(), drones[1].transform)});
+        // tail
+        links.Add(new ImpedanceController[] {
+            SetupLink(drones[3].AddComponent<ImpedanceController>(), drones[1].transform),
+            SetupLink(drones[3].AddComponent<ImpedanceController>(), drones[2].transform)
+        });
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3[] desired = {
+            SwarmCentroid.InverseTransformPoint(nearDestination.transform.position),
+            drones[0].transform.localPosition,
+            drones[0].transform.localPosition,
+            0.5f*(drones[1].transform.localPosition + drones[2].transform.localPosition)
+        };
+        for (int i = 0; i < links.Count; ++i) {
+            UpdateGoal(drones[i], links[i], desired[i], desiredDistances[i]);
+        }
+        RotateSwarm(20f * Time.deltaTime);
+    }
+
+    void UpdateGoal(
+        GameObject drone,
+        ImpedanceController[] links,
+        Vector3 desiredPosition,
+        Vector2 desiredDistance)
+    {
+        Vector3 correctionPos = Vector3.zero, correctionVel = Vector3.zero;
+        foreach(var link in links) {
+            (var onePosCorrection, var oneVelCorrection) = link.GetCorrection();
+            correctionPos += onePosCorrection;
+            correctionVel += oneVelCorrection;
+        }
+        correctionPos.x = -Mathf.Abs(correctionPos.x);
+        desiredPosition.x -= desiredDistance.x;
+        desiredPosition.z += desiredDistance.y;
+        // drone.transform.Translate()
+        drone.transform.localPosition = desiredPosition + correctionPos;
+        // var rb = drone.GetComponent<Rigidbody>();
+        // rb.AddForce(correctionVel * Mass, ForceMode.VelocityChange);
+        // rb.velocity.Normalize();
+    }
+
+    void RotateSwarm(float eulerYaw) {
+        SwarmCentroid?.Rotate(0f, eulerYaw, 0f);
+    }
+    ImpedanceController SetupLink(ImpedanceController link, Transform obstacle, bool logs=false) 
+    {
+        link.printLogs = logs;
+        link.obstacle = obstacle;
+        link.mass = Mass;
+        link.damping = Damping;
+        link.stiffness = Stiffness;
+        link.kGain = Gain;
+        link.correctionLimit = defaultCorrectionLimit;
+        return link;
+    }
+
+    void UpdateLinksParameter(System.Action<ImpedanceController> callback)
+    {
+        foreach(var drone_links in links) {
+            foreach(var link in drone_links) {
+                callback(link);
+            }
+        }
+    }
+}
