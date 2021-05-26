cc49571e29b74656b8df13b348d78dcadfa48825
ShahzebAamir <84467204+ShahzebAamir@users.noreply.github.com>
2021-05-26 17:31:41 +0500

    Add files via upload

--- Assets/Scripts/MoveToGoalAgent.cs
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -10,15 +10,18 @@ public class MoveToGoalAgent : Agent
 {
     public float speed = 100;
     public Transform Target;
+    public Transform Ref;
     public Transform[] obs;
-    public float forceMultiplier = 1f;
+    public float forceMultiplier = 0.01f;
     [SerializeField] private Material win;
+    [SerializeField] private Material close;
     [SerializeField] private Material lose;
     [SerializeField] private MeshRenderer floor;
     Vector3 goalPosition;
     private int counter = 0;
     private float time = 0;
-    
+    //private float prevDistance;
+
 
     Rigidbody rBody;
     void Start()
@@ -28,23 +31,25 @@ public class MoveToGoalAgent : Agent
 
     public override void OnEpisodeBegin()
     {
-            time = 0;
-            // If the Agent fell, zero its momentum
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3(0.33f, 0.5f, 2.54f);
-            
+        time = 0;
+        // If the Agent fell, zero its momentum
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        this.transform.localPosition = new Vector3(0.33f, 0.5f, 2.54f);
+        //float prevDistance = Vector3.Distance(this.transform.position, Target.transform.position);
 
 
-        //Target[0].localPosition = new Vector3(-1.5f, 0.54f, -1.54f);
+
+        //Target.localPosition = new Vector3(-1.5f, 0.54f, -1.54f);
         // Move the target to a new spot
         //So that target and obstacle not collide
+   
         bool goalspawned = false;
         while (!goalspawned)
         {
             goalPosition = new Vector3(Random.Range(-2.75f, 2.75f),
-                                              0.5f,
-                                               Random.Range(-2.75f, -1f));
+                                              Random.Range(0.5f, 0.6f),
+                                               Random.Range(-2.75f, 2.75f));
             if ((goalPosition - obs[0].localPosition).magnitude < 1 || (goalPosition - obs[1].localPosition).magnitude < 1 || (goalPosition - obs[2].localPosition).magnitude < 1 || (goalPosition - obs[3].localPosition).magnitude < 1)
             {
                 continue;
@@ -57,14 +62,11 @@ public class MoveToGoalAgent : Agent
         }
         //If to try moving obstacle
         //obs[0].localPosition = new Vector3(Random.Range(-2.5f, 2.5f), 0.5f, -0.5f);
-
+        //Debug.Log(prevDistance);
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-
-
-        // Actions, size = 2
         Vector3 controlSignal = Vector3.zero;
         controlSignal.x = actionBuffers.ContinuousActions[0];
         controlSignal.y = actionBuffers.ContinuousActions[1];
@@ -73,22 +75,21 @@ public class MoveToGoalAgent : Agent
         //rBody.mass* Physics.gravity.magnitude = hovering case
         //rBody.mass* Physics.gravity.magnitude + controlSignal.y * forceMultiplier = Thrust case
 
-        rBody.AddForce(controlSignal.x * forceMultiplier, rBody.mass*Physics.gravity.magnitude, controlSignal.z * forceMultiplier,ForceMode.Force);
+        rBody.AddForce(controlSignal.x * forceMultiplier, rBody.mass * Physics.gravity.magnitude + controlSignal.y * 0.25f * forceMultiplier, controlSignal.z * forceMultiplier, ForceMode.Force);
 
         // Moving to target
         //float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
         time += Time.deltaTime;
 
         //Limits of playfield and height
-        if (this.transform.localPosition.x < -2.85f || this.transform.localPosition.x > 2.85f || this.transform.localPosition.z < -2.85f || this.transform.localPosition.z > 2.85f || this.transform.localPosition.y < 0.25f || this.transform.localPosition.y > 2f || time > 60f)
+        if (this.transform.localPosition.x < -2.85f || this.transform.localPosition.x > 2.85f || this.transform.localPosition.z < -2.85f || this.transform.localPosition.z > 2.85f || this.transform.localPosition.y < 0.25f || this.transform.localPosition.y > 0.75f || time > 60f)
         {
             SetReward(-1.0f);
             floor.material = lose;
             EndEpisode();
-            counter++;
-            Debug.Log(counter);
         }
-    } 
+
+    }
 
     //rewards
     //We want to add more policy here to fasten the training process
@@ -100,18 +101,17 @@ public class MoveToGoalAgent : Agent
             SetReward(5.0f);
             floor.material = win;
             EndEpisode();
+            Debug.Log("Yes");
         }
 
         if (other.gameObject.tag == "Obstacle")
         {
-            SetReward(-1.0f);
+            SetReward(-0.5f);
             floor.material = lose;
             EndEpisode();
-            counter++;
-            Debug.Log(counter);
         }
-
     }
+
 
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -123,6 +123,10 @@ public class MoveToGoalAgent : Agent
         sensor.AddObservation(rBody.velocity.x);
         sensor.AddObservation(rBody.velocity.y);
         sensor.AddObservation(rBody.velocity.z);
+
+
+        
+
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -130,6 +134,15 @@ public class MoveToGoalAgent : Agent
         var continuousActionsOut = actionsOut.ContinuousActions;
         continuousActionsOut[0] = -Input.GetAxis("Horizontal");
         continuousActionsOut[2] = -Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.W))
+        {
+            continuousActionsOut[1] = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            continuousActionsOut[1] = -1;
+        }
+         
     }
 }
 
